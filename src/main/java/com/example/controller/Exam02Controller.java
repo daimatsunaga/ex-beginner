be7d3861b84fc6d7153b02062d4926dc5d41818d
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2021-11-01 17:31:21 +0900

    exam03コントローラー修正

--- src/main/java/com/example/controller/Exam02Controller.java
+++ src/main/java/com/example/controller/Exam02Controller.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,7 +19,7 @@ public class Exam02Controller {
 	}
 	
 	@RequestMapping("/sum")
-	public String putName(Model model, Integer num1, Integer num2) {
+	public String sum(Integer num1, Integer num2) {
 		int result = num1 + num2;
 		session.setAttribute("num1", num1);
 		session.setAttribute("num2", num2);
