113c342567a8d08267a8ab7b6324f3071d5ab085
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2021-11-01 16:48:22 +0900

    exam01-resul.htmlの作成

--- src/main/java/com/example/controller/Exam01Controller.java
+++ src/main/java/com/example/controller/Exam01Controller.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +11,11 @@ public class Exam01Controller {
 	@RequestMapping("")
 	public String index() {
 		return "exam01";
+	}
+	
+	@RequestMapping("put-name")
+	public String putName(Model model, String name) {
+		model.addAttribute("name", name);
+		return "exam01-result";
 	}
 }
