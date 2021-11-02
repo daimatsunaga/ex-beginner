6e58ab40994f4e0cfc8d68106fd1919a4c4937bf
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2021-11-02 09:22:03 +0900

    フォームの作成

---
+++ src/main/java/com/example/form/infoForm.java
@@ -0,0 +1,35 @@
+package com.example.form;
+
+public class infoForm {
+	
+	private String name;
+	private String age;
+	private String comment;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "infoForm [name=" + name + ", age=" + age + ", comment=" + comment + "]";
+	}
+	
+}
