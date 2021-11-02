7cc4f52c59b17ace66c8c82e094403219ca5fa41
daimatsunaga <dai.matsunaga@rakus-partners.co.jp>
2021-11-02 09:09:53 +0900

    applicationスコープに変更

--- src/main/java/com/example/controller/Exam03Controller.java
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -1,12 +1,17 @@
 package com.example.controller;
 
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/exam03")
 public class Exam03Controller {
+	
+	@Autowired
+	private ServletContext application;
 	
 	@RequestMapping("")
 	public String index() {
@@ -14,11 +19,11 @@ public class Exam03Controller {
 	}
 	
 	@RequestMapping("/total")
-	public String total(Integer num1, Integer num2, Integer num3, Model model) {
+	public String total(Integer num1, Integer num2, Integer num3) {
 		Integer result = num1 + num2 + num3;
 		Integer result2 = (int) (result * 1.1);
-		model.addAttribute("result", result);
-		model.addAttribute("result2", result2);
+		application.setAttribute("result", result);
+		application.setAttribute("result2", result2);
 		return "exam03-result";
 	}
 }
